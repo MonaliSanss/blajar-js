480296c676ba9b5730450f264fddb0030660a348
Maul <aryacrozz@gmail.com>
2025-09-08 13:37:32 +0700

    coba upload

---
+++ test.js
@@ -0,0 +1,20 @@
+// belajar algoritma
+
+/*
+
+ALGORITMA BY DESCRITION
+
+cara order suatu barang di website
+
+langkah awal
+pililah barang yang akan kamu beli
+lalu kamu bisa memntukan jumblah barang yang kamu beli
+
+ketika kamu sudah yakin dengan apa yang kamu order
+kamu bisa menekan/click checkout di bawah
+
+
+JADI MUDAHNYA ITU ALGORITMA ADALAH LANGKAH-LANGKAH MELAKUKAN SUATU KEGIATAN DAN BISA JUGA UNTUK LANGKAH-LANGKAH MENYESELAIKAN SUATU MASALAH
+AGAR KITA PAHAM STEP BY STEPNYA
+
+*/
